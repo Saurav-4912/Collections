1e1b275a22cadbefd768e1b2d8ed32b1b1505eda
Saurav Pawar <sauravpawar46@gmail.com>
2025-08-16 11:01:23 +0530

    Added LinkedHaspMap Class Code

---
+++ src/main/java/com/tca/map/LinkedHashMap_1.java
@@ -0,0 +1,25 @@
+package com.tca.map;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedHashMap_1 {
+
+    public static void main(String[] args) {
+
+        LinkedHashMap<Integer,String> lhm = new LinkedHashMap<>();
+
+        lhm.put(101,"AAA");
+        lhm.put(102,"BBB");
+        lhm.put(103,"CCC");
+        lhm.put(104,"DDD");
+
+        // Simple way: for-each loop
+        for (Map.Entry<Integer, String> e : lhm.entrySet()) {
+            System.out.println(e.getKey() + " -> " + e.getValue());
+        }
+
+    }
+}
